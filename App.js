a70adb9398d3d2df334102d0cdaa0b6badd4b1ee
Yaroslav Sedyukov <sedyukovofficial@gmail.com>
2022-03-04 18:05:17 +0300

    redux slices added

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux"
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={}>
+      <View style={styles.container}>
+        <Text>Ld UBfdER</Text>
+        <Text>Ld UBfdER</Text>
+      </View>
+    </Provider>
   );
 }
 
---
+++ slices/navSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null,
+}
+
+export const navSlice = createSlice({
+    name: 'nav',
+    initialState,
+    reducer: {
+        setOrigin: (state, action) => {
+            state.origin = action.payload;
+        },
+        setDestination: (state, action) => {
+            state.Destination = action.payload;
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.travelTimeInformation = action.payload;
+        },
+    },
+});
+
+export const { setOrigin, setDestination,
+    setTravelTimeInformation } = navSlice.actions;
